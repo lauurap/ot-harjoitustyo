32c34d3e42cf1f3eb9a48307809002d03bbcc14d
Laura Palo <laura.palo@outlook.com>
2022-11-28 14:26:32 +0200

    tasks

--- tasks.py
+++ tasks.py
@@ -9,7 +9,7 @@ def test(ctx):
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest src", pty=True)
+	ctx.run("coverage run --branch -m pytest src", pty=True)
 
 @task(coverage)
 def coverage_report(ctx):
