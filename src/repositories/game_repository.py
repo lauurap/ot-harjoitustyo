5dff91adf7c01711de7ac5a43156bc0f48995614
Laura Palo <laura.palo@outlook.com>
2022-12-11 17:37:31 +0200

    docstring

--- src/repositories/game_repository.py
+++ src/repositories/game_repository.py
@@ -1,9 +1,26 @@
 class GameRepository:
+    """Luokka, jonka avulla tallennetaan peli ja luetaan tallennettu peli.
+
+    Attributes:
+        name1 = Pelaajan 1 nimi
+        name2 = Pelaajan 2 nimi
+    """
+
     def __init__(self, name1, name2):
+        """Luokan konstruktori, joka luo uuden GameRepository-olion.
+
+        Args:
+            name1 = Pelaajan 1 nimi
+            name2 = Pelaajan 2 nimi
+        """	
+        
         self.name1 = name1
         self.name2 = name2
 
     def store(self, numbers, turn):
+        """Tallentaa pelaajat, pelilaudan ja seuraavana vuorossa olevan pelaajan vuoron.
+        """
+
         with open("stored_games.txt", "w") as file:
             board = ""
             for i in range(10):
@@ -12,14 +29,18 @@ class GameRepository:
             file.write(self.name2 + "\n")
             file.write(board + "\n")
             file.write(str(turn))
-        # file.close()
         print("Tallennus onnistui")
 
     def read(self):
+        """Lukee tiedostosta tallenetun pelin palaajat, pelilaudan ja seuraavana vuorossa olevan pelaajan vuoron.
+
+        Returns:
+            Listan, jossa on pelaajien nimet, pelilauta ja pelivuoro.
+        """
+
         with open("stored_games.txt", "r") as file:
             name1 = file.readline().strip()
             name2 = file.readline().strip()
             board = file.readline().strip()
             turn = file.readline().strip()
-        # file.close()
         return [name1, name2, board, turn]
--- src/services/humanplayer.py
+++ src/services/humanplayer.py
@@ -1,13 +1,42 @@
 class HumanPlayer:
+    """Luokka, joka tekee ihmispelaajan pelivuoroja
+
+    Attributes:
+        name: Ihmispelaajan nimi
+        mark: Ihmispelaajan merkki
+    """
+
     def __init__(self, name, mark):
+        """Luokan konstruktori, joka luo uuden ihmispelaajan.
+
+        Args:
+            name: Ihmispelaajan nimi
+            mark: Ihmispelaajan merkki
+        """
+
         self.name = name
         self.mark = mark
 
     def do_turn(self, board, place):
+        """Asettaa merkin pelilaudalle.
+
+        Returns:
+            True, jos merkin asetus onnistuu, muussa tapauksessa False.
+        """
         success = board.set_mark(self.mark, place)
         return success
 
     def ask_place(self, board, turn):
+        """Kysyy pelaajalta, mihin pelaaja haluaa asettaa merkin.
+
+        Args: 
+            board: Pelilauta, jonne merkki asetetaan
+            turn: Vuorossa olevan pelaajan vuoro
+	
+        Returns:
+            place eli paikan, jos paikka hyväksytään
+        """
+
         correct_place = False
         while correct_place is False:
             place = input("Mihin paikkaan haluat asettaa merkkisi? \n")
@@ -15,6 +44,9 @@ class HumanPlayer:
         return int(place)
 
     def do_turns(self, board, turn):
+        """Vastaa paikan kysymisestä ja pelivuoron suorittamisesta.
+        """
+
         print("Vuorossa on", self.name)
         place = self.ask_place(board, turn)
         while self.do_turn(board, place) is False:
