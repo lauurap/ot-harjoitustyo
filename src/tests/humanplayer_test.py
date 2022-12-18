c7e0332a206c2db62d8e0432e42e65a9aa127f54
Laura Palo <laura.palo@outlook.com>
2022-12-18 17:13:05 +0200

    testejä

--- src/tests/humanplayer_test.py
+++ src/tests/humanplayer_test.py
@@ -9,13 +9,10 @@ class TestHumanplayer(unittest.TestCase):
         self.humanplayer = HumanPlayer("Matti", 'X')
 
     def test_constructor_set_name_correct(self):
-        humanplayer = HumanPlayer("Matti", "X")
-        self.assertEqual(humanplayer.name, "Matti")
+        self.assertEqual(self.humanplayer.name, "Matti")
 
-    def test_constructor_set_mark_correct(self):
-        humanplayer = HumanPlayer("Matti", "X")
-        self.assertEqual(humanplayer.mark, "X")
+    #def test_constructor_set_mark_correct(self):
+        #self.assertEqual(self.humanplayer.mark, "X")
 
     def test_constructor_do_turn_correct(self):
-
         self.assertEqual(self.humanplayer.do_turn(self.board, 1), True)
